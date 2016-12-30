004a136fc9d298f73c4a69a5ffc9e1f8b520fac8
xieweiting <xieweiting@admaster.com.cn>
2016-12-30 10:40:24 +0800

    添加 push.sh 文件方便提交

--- push.sh
+++ push.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
 
-git add . ;git commit -am 'add'; git push
+read -p "请输入 commit 信息(n 取消; 回车默认): " commitMsg
+if [ "$commitMsg" == 'n' ]
+then
+  echo '操作取消...'
+  exit 0
+fi
+
+if [ "$commitMsg" == '' ]
+then
+  commitMsg='add something...'
+fi
+
+echo "Exec: git commit -am $commitMsg"
+
+git add .
+
+git commit -am "$commitMsg"
+
+git push
+
+echo 'done...'
